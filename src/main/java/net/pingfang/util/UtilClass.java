6edd42a68d32037fda1146aa7662b5a9ac265115
VMHOST\何达宏 <hedh@pingfang.net>
2018-03-26 16:14:16 +0800

    工具类的编写

--- src/main/java/net/pingfang/util/UtilClass.java
+++ src/main/java/net/pingfang/util/UtilClass.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -122,4 +123,59 @@ public class UtilClass {
 		String uuid = UUID.randomUUID().toString().trim().replace("-", "");
 		return uuid.toLowerCase();
 	}
+    /**
+     * md5加密
+     * @param password
+     * @return
+     */
+	public static String md5Encode(String password) {
+		MessageDigest md5 = null;
+		
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+		} catch (Exception e) {
+			throw new RuntimeException();
+		}
+		char[] charArray = password.toCharArray();
+		byte[] byteArray = new byte[charArray.length];
+		
+		
+		for(int i=0; i < charArray.length; i++) 
+			byteArray[i] = (byte) charArray[i];
+		byte[] md5Bytes = md5.digest(byteArray);
+		StringBuffer hexValue = new StringBuffer();
+		for(int i =0; i < md5Bytes.length; i++) {
+			int val =((int)md5Bytes[i])&0xff;
+			if(val < 16) {
+				hexValue.append("0");
+			}
+			hexValue.append((Integer.toHexString(val)));
+		}
+		return hexValue.toString();
+	}
+	/**
+     * 获取图片类型
+     * @param f
+     * @return
+     * @throws Exception
+     */
+	public static String getFileType(File f) throws Exception{
+        ImageInputStream iis = null;
+        try {
+            // create an image input stream from the specified fileDD
+            iis = ImageIO.createImageInputStream(f);
+            // get all currently registered readers that recognize the image format
+            Iterator<ImageReader> iter = ImageIO.getImageReaders(iis);
+            if (!iter.hasNext()) {
+                throw new Exception("Throw Exception when getFileType: No readers found!");
+            }
+            // get the first reader
+            ImageReader reader = iter.next();
+            return reader.getFormatName();
+        }finally {
+            // close stream
+            iis.close();
+        }
+
+    }
 }
